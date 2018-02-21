518844e12c424564f33b1e0704e87a757b4c1ff0
tar-xzvff <jv120228m@gmail.com>
2018-02-21 15:41:14 +0900

    更新日時と追加日時を保存するように変更

--- terraform_manager/common/models/terraform_file.py
+++ terraform_manager/common/models/terraform_file.py
@@ -3,3 +3,5 @@ from django.db import models
 
 class TerraformFile(models.Model):
     body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
