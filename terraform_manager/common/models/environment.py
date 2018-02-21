b795260756e7229fad5eb30c4282a81e48758531
tar-xzvff <jv120228m@gmail.com>
2018-02-21 16:09:30 +0900

    TerraformFileモデルへのリレーションの設定を追加

--- terraform_manager/common/models/environment.py
+++ terraform_manager/common/models/environment.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-from models.terraform_file import TerraformFile
+from common.models.terraform_file import TerraformFile
 
 
 class Environment(models.Model):
-    terraform_file = models.OneToOneField(TerraformFile)
+    terraform_file = models.ForeignKey(TerraformFile, unique=False, on_delete=True)
+    state = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
