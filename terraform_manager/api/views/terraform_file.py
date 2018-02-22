83f93d081537693d6a143fe08c00d550f02359a0
tar-xzvff <jv120228m@gmail.com>
2018-02-23 08:41:04 +0900

    environmentを作成するビューメソッドを追加

--- terraform_manager/api/views/terraform_file.py
+++ terraform_manager/api/views/terraform_file.py
@@ -4,11 +4,26 @@ from common.models.terraform_file import TerraformFile
 from common.models.environment import Environment
 
 from api.serializers.terraform_file import TerraformFileSerializer
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 
 
 class TerraformFileViewSet(viewsets.ModelViewSet):
     queryset = TerraformFile.objects.all()
     serializer_class = TerraformFileSerializer
+
+    @detail_route(methods=['post'])
+    def create_environment(self, *args, **kwargs):
+        from api.serializers.environment import EnvironmentSerializer
+        data = {"terraform_file": kwargs['pk']}
+        serializer = EnvironmentSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            print("VALID")
+            return Response()
+        else:
+            print("INVALID")
+            return Response()
 
 
 class EnvironmentViewSet(viewsets.ModelViewSet):
