18051908461559152fa706af859c3ad485b74102
tar-xzvff <jv120228m@gmail.com>
2018-02-28 18:26:36 +0900

    celeryを組み込む設定を追加

--- terraform_manager/api/views/terraform_file.py
+++ terraform_manager/api/views/terraform_file.py
@@ -4,6 +4,7 @@ from common.models.terraform_file import TerraformFile
 from common.models.environment import Environment
 
 from api.serializers.terraform_file import TerraformFileSerializer
+from rest_framework.response import Response
 
 
 class TerraformFileViewSet(viewsets.ModelViewSet):
@@ -14,3 +15,10 @@ class TerraformFileViewSet(viewsets.ModelViewSet):
 class EnvironmentViewSet(viewsets.ModelViewSet):
     queryset = Environment.objects.all()
     serializer_class = TerraformFileSerializer
+
+
+class CeleryExampleViewSet(viewsets.ViewSet):
+    def list(self, request):
+        from common.common_tasks import debug_task
+        debug_task.delay()
+        return Response()
