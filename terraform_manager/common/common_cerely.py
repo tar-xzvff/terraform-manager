e2727ab058f38c1648691a5d6dc8d115b2c9906a
tar-xzvff <jv120228m@gmail.com>
2018-02-28 18:03:00 +0900

    ファイル追加

---
+++ terraform_manager/common/common_cerely.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'terraform_manager.settings')
+
+app = Celery('terraform_manager')
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
+
+def get_app():
+    return app
