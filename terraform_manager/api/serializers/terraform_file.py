b66c36426c5c527906adae4996d12df7b455e175
tar-xzvff <jv120228m@gmail.com>
2018-03-01 15:27:57 +0900

    TerraformFileでnameを設定できるように変更

--- terraform_manager/api/serializers/terraform_file.py
+++ terraform_manager/api/serializers/terraform_file.py
@@ -6,4 +6,4 @@ from common.models.terraform_file import TerraformFile
 class TerraformFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = TerraformFile
-        fields = ('id', 'body', 'created_at', 'updated_at', )
+        fields = ('id', 'name', 'body', 'created_at', 'updated_at', )
