22c210ebfd370a82cc90d984460e1315adb7e5b3
tar-xzvff <jv120228m@gmail.com>
2018-03-01 17:05:55 +0900

    Environment作成時にterraformを実行するディレクトリとファイルを作成する処理を追加

--- terraform_manager/api/serializers/environment.py
+++ terraform_manager/api/serializers/environment.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
 
-from common.models.terraform_file import TerraformFile
 from common.models.environment import Environment
 
 
 class EnvironmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Environment
-        fields = ('id', 'terraform_file', )
+        fields = ('terraform_file', )
         read_only_fields = ('id', 'created_at', 'updated_at', )
 
     def create(self, validated_data):
-        return Environment(**validated_data).save()
+        environment = Environment.objects.create(**validated_data)
+        from common.common_tasks import copy_tf_files
+        copy_tf_files.delay(environment.id, environment.terraform_file.id)
+        return environment
