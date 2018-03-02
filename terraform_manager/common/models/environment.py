7984ecd015578367d10c462cee256b3be5f2ed2c
tar-xzvff <jv120228m@gmail.com>
2018-03-02 14:32:03 +0900

    Environmentモデルにロック状態を格納する変数を追加

--- terraform_manager/common/models/environment.py
+++ terraform_manager/common/models/environment.py
@@ -6,5 +6,6 @@ from common.models.terraform_file import TerraformFile
 class Environment(models.Model):
     terraform_file = models.ForeignKey(TerraformFile, unique=False, on_delete=True)
     state = models.CharField(max_length=20)
+    locked = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
