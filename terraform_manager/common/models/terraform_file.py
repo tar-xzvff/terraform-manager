cdd2246b045f0debe35c955809223d9fddb27c87
tar-xzvff <jv120228m@gmail.com>
2018-03-02 15:06:05 +0900

    idをUUIDに変更

--- terraform_manager/common/models/terraform_file.py
+++ terraform_manager/common/models/terraform_file.py
@@ -1,8 +1,12 @@
+import uuid
+
 from django.db import models
 
 
 class TerraformFile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
     body = models.TextField()
+    file_name = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
