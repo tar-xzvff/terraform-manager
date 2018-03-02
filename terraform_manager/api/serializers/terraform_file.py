a82a1026e1b49e66f3c1ea217dcd9f66ed8b4ab7
tar-xzvff <jv120228m@gmail.com>
2018-03-02 13:26:23 +0900

    ファイル削除

--- terraform_manager/api/serializers/terraform_file.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from common.models.terraform_file import TerraformFile
-
-
-class TerraformFileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TerraformFile
-        fields = ('id', 'name', 'body', 'created_at', 'updated_at', )
