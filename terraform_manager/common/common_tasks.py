0d0d776ecd377a6d1b90453a7b8c15d7ece0720e
tar-xzvff <jv120228m@gmail.com>
2018-04-12 11:24:14 +0900

    共通処理の共通化

--- terraform_manager/common/common_tasks.py
+++ terraform_manager/common/common_tasks.py
@@ -3,6 +3,7 @@ import os, logging
 from celery import Celery
 from python_terraform import Terraform
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'terraform_manager.settings')
 
 app = Celery('terraform_manager')
@@ -29,26 +30,7 @@ def copy_tf_files(environment_id, terraform_file_id):
     :param environment_id:  環境ID
     :param terraform_file_id:   terraformファイルID
     """
-    environment_dir = TERRAFORM_ENVIRONMENT_ROOT_PATH + str(environment_id)
-
-    import os
-    os.mkdir(environment_dir)
-
-    from common.models.terraform_file import TerraformFile
-    tf = TerraformFile.objects.get(id=terraform_file_id)
-    f = open(environment_dir + "/" + '{}.tf'.format(tf.name), 'w')
-    f.writelines(tf.body)
-    f.close()
-
-    variables_tf = """
-variable "token" {}
-variable "secret" {}
-variable "zone" {}
-    """
-
-    f = open(environment_dir + "/" + '{}.tf'.format("variables"), 'w')
-    f.writelines(variables_tf)
-    f.close()
+    prepare_environment(environment_id, terraform_file_id)
 
 
 @app.task
@@ -59,6 +41,9 @@ def init(environment_id):
     """
     from common.models.environment import Environment
     environment = Environment.objects.get(id=environment_id)
+
+    if not os.path.isdir(TERRAFORM_ENVIRONMENT_ROOT_PATH + environment_id):
+        prepare_environment(environment_id=environment_id, terraform_file_id=environment.terraform_file.id)
     environment.locked = True
     environment.save()
     try:
@@ -68,13 +53,7 @@ def init(environment_id):
         #   TODO    :   エラーログを送出する
         pass
     finally:
-        from common.models import Log
-        from common.models.environment import Environment
-        log = Log(environment=Environment.objects.get(id=environment_id),
-                  return_code=return_code,
-                  stdout=stdout,
-                  stderr=stderr)
-        log.save()
+        save_log(environment_id, return_code, stdout, stderr)
         environment.locked = False
         environment.save()
 
@@ -86,27 +65,27 @@ def plan(environment_id, var):
     :param environment_id:  環境ID
     :param var: terraformコマンド実行時に引数に渡す変数
     """
-    if not os.path.isdir(TERRAFORM_ENVIRONMENT_ROOT_PATH + environment_id):
-        #   TODO    :   initとcopy_tf_files関数と同じ処理を入れる
-        pass
     from common.models.environment import Environment
     environment = Environment.objects.get(id=environment_id)
+
+    if not os.path.isdir(TERRAFORM_ENVIRONMENT_ROOT_PATH + environment_id):
+        prepare_environment(environment_id, environment.terraform_file.id)
+        init(environment_id)
+    if environment.locked:
+        # ロックしているときはコマンドを実行しない.
+        raise Exception()
+
     environment.locked = True
     environment.save()
     try:
+        print(var)
         tf = Terraform(working_dir=TERRAFORM_ENVIRONMENT_ROOT_PATH + str(environment_id))
         return_code, stdout, stderr = tf.plan(var=var)
     except:
         #   TODO    :   エラーログを送出する
         pass
     finally:
-        from common.models import Log
-        from common.models.environment import Environment
-        log = Log(environment=Environment.objects.get(id=environment_id),
-                  return_code=return_code,
-                  stdout=stdout,
-                  stderr=stderr)
-        log.save()
+        save_log(environment_id, return_code, stdout, stderr)
         environment.locked = False
         environment.save()
 
@@ -118,11 +97,16 @@ def apply(environment_id, var):
     :param environment_id:  環境ID
     :param var: terraformコマンド実行時に引数に渡す変数
     """
-    if not os.path.isdir(TERRAFORM_ENVIRONMENT_ROOT_PATH + environment_id):
-        #   TODO    :   initとcopy_tf_files関数と同じ処理を入れる
-        pass
     from common.models.environment import Environment
     environment = Environment.objects.get(id=environment_id)
+
+    if not os.path.isdir(TERRAFORM_ENVIRONMENT_ROOT_PATH + environment_id):
+        prepare_environment(environment_id, environment.terraform_file.id)
+        init(environment_id)
+    if environment.locked:
+        # ロックしているときはコマンドを実行しない.
+        raise Exception()
+
     environment.locked = True
     environment.save()
     try:
@@ -132,13 +116,7 @@ def apply(environment_id, var):
         #   TODO    :   エラーログを送出する
         pass
     finally:
-        from common.models import Log
-        from common.models.environment import Environment
-        log = Log(environment=Environment.objects.get(id=environment_id),
-                  return_code=return_code,
-                  stdout=stdout,
-                  stderr=stderr)
-        log.save()
+        save_log(environment_id, return_code, stdout, stderr)
         environment.locked = False
         environment.save()
 
@@ -150,11 +128,16 @@ def destroy(environment_id, var):
     :param environment_id:  環境ID
     :param var: terraformコマンド実行時に引数に渡す変数
     """
-    if not os.path.isdir(TERRAFORM_ENVIRONMENT_ROOT_PATH + environment_id):
-        #   TODO    :   initとcopy_tf_files関数と同じ処理を入れる
-        pass
     from common.models.environment import Environment
     environment = Environment.objects.get(id=environment_id)
+
+    if not os.path.isdir(TERRAFORM_ENVIRONMENT_ROOT_PATH + environment_id):
+        prepare_environment(environment_id, environment.terraform_file.id)
+        init(environment_id)
+    if environment.locked:
+        # ロックしているときはコマンドを実行しない.
+        raise Exception()
+
     environment.locked = True
     environment.save()
     try:
@@ -164,13 +147,60 @@ def destroy(environment_id, var):
         #   TODO    :   エラーログを送出する
         pass
     finally:
-        from common.models import Log
-        from common.models.environment import Environment
-        log = Log(environment=Environment.objects.get(id=environment_id),
-                  return_code=return_code,
-                  stdout=stdout,
-                  stderr=stderr)
-        log.save()
+        save_log(environment_id, return_code, stdout, stderr)
         environment.locked = False
         environment.save()
 
+
+def prepare_environment(environment_id, terraform_file_id):
+    """
+    terraform実行環境を作成します.
+    :param environment_id:  環境ID
+    :param terraform_file_id:   TerraformファイルID
+    """
+
+    # ディレクトリの作成.
+    environment_dir = TERRAFORM_ENVIRONMENT_ROOT_PATH + str(environment_id)
+    os.mkdir(environment_dir)
+
+    # *.tfファイルのコピー.
+    from common.models.terraform_file import TerraformFile
+    tf = TerraformFile.objects.get(id=terraform_file_id)
+    f = open(environment_dir + "/" + '{}.tf'.format(tf.name), 'w')
+    f.writelines(tf.body)
+    f.close()
+
+    # 変数定義ファイルの作成.
+    variables_tf = """
+provider "sakuracloud" {
+    # APIキー(トークン)
+    token = "${var.token}"
+    # APIキー(シークレット)
+    secret = "${var.secret}"
+    # デフォルトゾーン
+    zone = "${var.zone}"
+}
+variable "token" {}
+variable "secret" {}
+variable "zone" {}
+    """
+    f = open(environment_dir + "/" + '{}.tf'.format("variables"), 'w')
+    f.writelines(variables_tf)
+    f.close()
+
+
+def save_log(environment_id, return_code, stdout, stderr):
+    """
+    ログを保存します.
+    :param environment_id:
+    :param return_code:
+    :param stdout:
+    :param stderr:
+    """
+    from common.models import Log
+    from common.models.environment import Environment
+    log = Log(environment=Environment.objects.get(id=environment_id),
+              return_code=return_code,
+              stdout=stdout,
+              stderr=stderr)
+    log.save()
