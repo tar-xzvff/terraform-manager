29ed602b399a788f186ddaaac12637ef77b30673
tar-xzvff <jv120228m@gmail.com>
2018-05-01 18:18:23 +0900

    STATEの定義を追加

--- terraform_manager/common/models/environment.py
+++ terraform_manager/common/models/environment.py
@@ -6,9 +6,22 @@ from common.models.terraform_file import TerraformFile
 
 
 class Environment(models.Model):
+    STATE_CHOICES = (
+        ('IN_WAITING_FOR_START', 'IN_WAITING_FOR_START'),
+        ('IN_FILE_COPYING', 'IN_FILE_COPYING'),
+        ('IN_INITIALIZE', 'IN_INITIALIZE'),
+        ('INITIALIZED', 'INITIALIZED'),
+        ('IN_PLANNING', 'IN_PLANNING'),
+        ('PLANNED', 'PLANNED'),
+        ('IN_APPLYING', 'IN_APPLYING'),
+        ('APPLIED', 'APPLIED'),
+        ('IN_DESTROYING', 'IN_DESTROYING'),
+        ('DESTROYED', 'DESTROYED'),
+        ('FAILED', 'FAILED'),
+    )
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     terraform_file = models.ForeignKey(TerraformFile, unique=False, on_delete=True)
-    state = models.CharField(max_length=20)
+    state = models.CharField(max_length=20, choices=STATE_CHOICES, default='IN_WAITING_FOR_START')
     locked = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
